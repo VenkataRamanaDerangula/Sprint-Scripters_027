806a81f46e117cbefcdf1feb756bf1c0129120dc
Venkata Ramana Derangula <114896262+VenkataRamanaDerangula@users.noreply.github.com>
2024-09-01 00:19:33 +0530

    changes in responsive  ...

--- index.js
+++ index.js
@@ -27,36 +27,45 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-// Login-logout functionality
+// // Login-logout functionality
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+
+//     if (loggedInUser) {
+//         const loginIcon = document.getElementById('loginIcon');
+//         const usernameDisplay = document.getElementById('usernameDisplay');
+//         const logoutButton = document.getElementById('logoutButton');
+
+//         // Hide the login icon
+//         if (loginIcon) loginIcon.style.display = 'none';
+
+//         // Display the username with styles
+//         if (usernameDisplay) {
+//             usernameDisplay.style.color = '#3498db';
+//             usernameDisplay.style.fontWeight = 'bold';
+//             usernameDisplay.textContent = `Hello, ${loggedInUser}!`;
+//         }
+
+//         // Show logout button
+//         if (logoutButton) {
+//             logoutButton.style.display = 'inline-block';
+//             logoutButton.addEventListener('click', function () {
+//                 // Remove the user from localStorage
+//                 localStorage.removeItem('loggedInUser');
+
+//                 // Redirect to the login page
+//                 window.location.href = 'index.html';
+//             });
+//         }
+//     } 
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+    const hamburger = document.querySelector('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav-links');
 
-    if (loggedInUser) {
-        const loginIcon = document.getElementById('loginIcon');
-        const usernameDisplay = document.getElementById('usernameDisplay');
-        const logoutButton = document.getElementById('logoutButton');
-
-        // Hide the login icon
-        if (loginIcon) loginIcon.style.display = 'none';
-
-        // Display the username with styles
-        if (usernameDisplay) {
-            usernameDisplay.style.color = '#3498db';
-            usernameDisplay.style.fontWeight = 'bold';
-            usernameDisplay.textContent = `Hello, ${loggedInUser}!`;
-        }
-
-        // Show logout button
-        if (logoutButton) {
-            logoutButton.style.display = 'inline-block';
-            logoutButton.addEventListener('click', function () {
-                // Remove the user from localStorage
-                localStorage.removeItem('loggedInUser');
-
-                // Redirect to the login page
-                window.location.href = 'index.html';
-            });
-        }
-    } 
+    hamburger.addEventListener('click', function () {
+        mobileNav.classList.toggle('show');
+    });
 });
