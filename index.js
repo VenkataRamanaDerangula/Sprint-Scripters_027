4da050ad0bfe70c54ed1024f66039b94addf0449
Venkata Ramana Derangula <114896262+VenkataRamanaDerangula@users.noreply.github.com>
2024-08-31 14:06:29 +0530

    files linked ...

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-// To changes images on banner
-const images = document.querySelectorAll('.banner img');
-let currentIndex = 0;
 
-function showNextImage() {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
-}
-setInterval(showNextImage, 2500);
-
-
-
-/// To redirect to login page 
-
+// To change banner images
 document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.banner img');
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+    }
+
+    // Initial setup to display the first image
+    images[currentIndex].classList.add('active');
+
+    // Set the interval for image rotation
+    setInterval(showNextImage, 2500);
+
+
+
+    // Redirect to login page
     document.getElementById("loginIcon").onclick = function() {
         window.location.href = "login.html";
     };
 });
-
